d857c1a8e110732a8b71d48a6431784eb190c7ca
Catherine Bandarchuk <kate@sealucky.com>
2023-02-16 13:02:18 -0800

    fixed bookshelf index.html

--- bookshelf/views.py
+++ bookshelf/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from book.models import Book
@@ -6,7 +7,7 @@ from book.models import Book
 @login_required
 def index(request):
     books = Book.objects.filter(owner_user_id = request.user)
-
-    return render(request,'bookshelf/index.html', {
-        'books': books,
-    })
+    paginator = Paginator(books, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'bookshelf/index.html', {'page_obj': page_obj})
