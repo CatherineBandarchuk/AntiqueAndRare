4fd00ef729a738c36eda46426c508c67faeea043
Catherine Bandarchuk <kate@sealucky.com>
2023-02-18 00:53:34 -0800

    added age group category model

--- book/models.py
+++ book/models.py
@@ -1,6 +1,17 @@
 # from django.contrib.auth.models import User
 from django.db import models
 
+class AgeGroupCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta: 
+        ordering = ('name',)
+        verbose_name_plural = 'Age Group Categories'
+
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -10,7 +21,6 @@ class Book(models.Model):
     age_group = models.CharField(max_length=255)    
     image = models.ImageField(upload_to='book_images', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    #owner_user_id = models.ForeignKey(User, related_name='books', on_delete=models.CASCADE)
     owner_user_id = models.ForeignKey('account.CustomUser', related_name='books', on_delete=models.CASCADE)
     available = models.BooleanField(default=True)
 
