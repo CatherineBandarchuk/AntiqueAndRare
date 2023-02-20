a4a15e928a9c4f70c7fc3e9338bc9f41886800e1
yves <dwu415@gmail.com>
2023-02-20 02:06:49 -0800

    add trade request page, currently submission not working

--- trades/urls.py
+++ trades/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'trades'
 
 urlpatterns = [
-    path('<int:pk>/', views.trade, name='trade'),
-    path('<int:pk>/select', views.select_book, name='select'),
+    #path('<int:pk>/', views.trade, name='trade'),
+    # path('<int:pk>/select', views.select_book, name='select'),
+    path('<int:pk>/', views.BookListView.as_view(), name='select'),
 ]
--- trades/views.py
+++ trades/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView
+from .forms import CreateRequestForm
 
 from .models import TradeRequest
 from book.models import Book
+from django.urls import reverse_lazy
 
 
-@login_required
-def trade(request, pk):
-    requested_book = get_object_or_404(Book, pk=pk)
-    return render(request, 'trades/option.html', { 'book': requested_book })
-
-
-class BookListView(ListView):
+class BookListView(generic.ListView, LoginRequiredMixin):
     model = Book
+    success_url = reverse_lazy("main:index")
+    context_object_name = 'book_list'
+    template_name = 'trades/option.html'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Book.objects.filter(available=True, owner_user_id=self.request.user)
+
+    def trade(request, pk):
+        requested_book = get_object_or_404(Book, pk=pk)
+        bookshelf = Book.objects.filter(owner_user_id=request.user)
+        offering_book = bookshelf.filter(pk=request.POST['choice'])
+
+        new_request = TradeRequest(
+            requested_book=requested_book,
+            offering_book=offering_book,
+        )
+
+        new_request.save()
+        return render(request, 'bookshelf/templates/bookshelf/index.html')
