117b3a0f76e2eee34bf0595dadeda600b4712a93
Catherine Bandarchuk <kate@sealucky.com>
2023-02-19 18:43:50 -0800

    Added multiple choices for search in bookshelf

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect 
+from django.shortcuts import render 
 from django.core.paginator import Paginator
 from django.db.models import Q
 from book.models import Book, AgeGroupCategory
